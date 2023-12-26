a956e6f598f39f0206731a191b0677ea5f36dc83
김민지 <blueseablueskyblueme@gmail.com>
2023-12-26 11:51:49 +0900

    [init] svgr 설정
    (cherry picked from commit ba6ccdf34adfde381ce0fdd39369b2d6c2e15677)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(), //svg를 react component로 사용하기 위한 플러그인
+  ],
 })
