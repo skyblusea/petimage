a64f66078ca2ad696c5737c81566724adb92d395
김민지 <blueseablueskyblueme@gmail.com>
2024-01-22 13:41:18 +0900

    [feat] TossWidget Prefetching / isAuth 상태 -> 함수로 교체(새로 고침 시 refCurrent 날라감에 대비

--- client/src/provider/AuthProvider.tsx
+++ client/src/provider/AuthProvider.tsx
@@ -1,9 +1,11 @@
 
-import { createContext, useEffect, useLayoutEffect, useReducer, useRef } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { createContext, useEffect, useReducer, useRef } from "react";
 import { apiClient, authClient } from "../util/axiosInstance";
-import Loading from "../components/Loading";
-import { Backdrop } from "@mui/material";
+import { QueryClient } from "@tanstack/react-query"
+import { tossWidgetQuery } from "../page/payment/checkout/page";
+import { Outlet, useNavigate } from "react-router-dom";
+
+
 
 // Context
 
@@ -12,22 +14,26 @@ type User = {
   name: string;
 } | undefined
 
-type Token = {
+export type Token = {
   access: string;
   refresh: string;
 } | undefined
 
-type AuthContextType = {
+export type AuthContextType = {
   user: User | null;
-  isAuthenticated: boolean;
+  isAuthenticated: () => void;
+  signInWithGoogle?: (response: google.accounts.id.CredentialResponse) => Promise<void>;
   // signInWithApple: () => Promise<void>;
   // signout: () => Promise<void>;
   logout: () => void;
+  token: Token | null;
 };
 
 const initialContextState: AuthContextType = {
   user: null,
-  isAuthenticated: false,
+  token: null,
+  isAuthenticated: () => { },
+  signInWithGoogle: () => Promise.resolve(),
   // signInWithApple: () => Promise.resolve(),
   // signout: () => Promise.resolve(),
   logout: () => { },
@@ -48,13 +54,11 @@ type AuthAction =
 type AuthState = {
   user: User;
   isAuthenticated: boolean;
-  isLoading: boolean;
 };
 
 const initialReducerState: AuthState = {
   user: undefined,
   isAuthenticated: false,
-  isLoading: false
 };
 
 const authReducer = (state: AuthState, action: AuthAction) => {
@@ -70,26 +74,41 @@ const authReducer = (state: AuthState, action: AuthAction) => {
 
 
 
-export default function AuthProvider() {
+export default function AuthProvider({ 
+  queryClient,
+}:{ 
+  queryClient : QueryClient 
+}) {
   const [state, dispatch] = useReducer(authReducer, initialReducerState);
-  const { user, isAuthenticated, isLoading } = state;
+  const { user } = state;
   const tokenRef = useRef<Token | undefined>();
   const navigate = useNavigate();
+  const isAuthenticated = () => {
+    tokenRefresh(tokenRef.current)
+  }
+
 
   useEffect(() => {
     console.log('init interceptor')
 
-    if (tokenRef.current === undefined) {
+    const prefetchingTossWidget = async (acessToken:string) => {
+      const query = tossWidgetQuery(acessToken)
+      await queryClient.prefetchQuery(query)
+    }
+    if (!tokenRef.current) {
       console.log('토큰 로컬에서 저장')
       const access = localStorage.getItem('access')
       const refresh = localStorage.getItem('refresh')
       if (access && refresh) {
         tokenRef.current = { access, refresh }
+        prefetchingTossWidget(access)
         dispatch({ type: 'LOGIN', user: { email: '', name: '' } })
       } else {
         dispatch({ type: 'LOGOUT' })
       }
     }
+
+  
 
     const authRequestInterceptor = authClient.interceptors.request.use(
       (config) => {
@@ -119,7 +138,7 @@ export default function AuthProvider() {
     );
 
     const apiResponseInterceptor = apiClient.interceptors.response.use(
-      (response) => {
+      async(response) => {
         // Cache new token from incoming response headers
         console.log('responseInterceptor 작동중')
         // 로그인 시에만 토큰 저장
@@ -131,6 +150,7 @@ export default function AuthProvider() {
           localStorage.setItem('access', accessToken);
           localStorage.setItem('refresh', refreshToken);
           tokenRef.current = { access: accessToken, refresh: refreshToken };
+          prefetchingTossWidget(accessToken)
         }
         return response;
       },
@@ -144,38 +164,26 @@ export default function AuthProvider() {
     };
   }, [tokenRef]);
 
-  useEffect(() => {
-    //https://developers.google.com/identity/gsi/web/reference/js-reference#google.accounts.id.initialize
-    //에 따라 mehtod 를 한번만 call 하고 인증 관련 함수를 중앙화 하기 위해 이동
-    const signInWithGoogle = async (response: google.accounts.id.CredentialResponse) => {
-      console.log('signInWithGoogle 실행중')
-      try {
-        const res = await apiClient.post('/user/google', { token: response.credential })
-        if (res.data.ok) {
-          alert('로그인 성공')
-          navigate('/')
-        }
-      } catch (error) {
-        console.log(error)
-        throw error;
+  const signInWithGoogle = async (response: google.accounts.id.CredentialResponse) => {
+    console.log('signInWithGoogle 실행중')
+    try {
+      const res = await apiClient.post('/user/google', { token: response.credential })
+      if (res.data.ok) {
+        alert('로그인 성공')
+        navigate('/')
       }
+    } catch (error) {
+      console.log(error)
+      throw error;
     }
-    const initGoogleAPIClient = () => {
-      console.log('init google api client')
-      google.accounts.id.initialize({
-        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-        callback: signInWithGoogle
-      })
-    }
-    initGoogleAPIClient();
-  }, [])
+  }
 
   const tokenRefresh = async (token: Token) => {
     console.log('토큰 리프레시 실행중')
     if (!token) {
       alert('토큰이 없습니다.')
       dispatch({ type: 'LOGOUT' })
-      navigate('/')
+      // navigate('/')
       return
     }
     authClient
@@ -190,18 +198,28 @@ export default function AuthProvider() {
         if (err.response.status === 401) {
           alert("로그인이 만료되었습니다.")
           dispatch({ type: 'LOGOUT' })
-          navigate("/")
+          // navigate("/")
         }
       });
   }
 
-  const logout = () => { }
+  const logout = () => {
+    localStorage.removeItem('access')
+    localStorage.removeItem('refresh')
+    dispatch({ type: 'LOGOUT' })
+    tokenRef.current = undefined
+    // navigate('/')
+    alert('로그아웃 되었습니다.')
+  }
+  // console.log('tokenRef', tokenRef.current)
 
 
   const context = {
     user,
+    token : tokenRef.current,
     isAuthenticated,
-    logout
+    signInWithGoogle,
+    logout,
   };
 
 
@@ -209,10 +227,6 @@ export default function AuthProvider() {
   return (
     <AuthContext.Provider value={context}>
       <Outlet />
-      <Backdrop open>
-        <Loading />
-      </Backdrop>
     </AuthContext.Provider>
   )
-
 }
