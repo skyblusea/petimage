d7019a62250921f69a762a284df248176290a529
김민지 <blueseablueskyblueme@gmail.com>
2024-01-23 10:15:48 +0900

    [design] 메인페이지 footer색 변환

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import styled from "@emotion/styled"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export default function Footer() {
+  const isHome = useLocation().pathname === '/'
+
   return (
-    <FooterContainer>
+    <FooterContainer isHome={isHome}>
       <Link to="#">개인정보처리방침</Link>
       <Link to="#">이용약관</Link>
       <Link to="#">문의하기</Link>
@@ -11,11 +13,14 @@ export default function Footer() {
   )
 }
 
+type FooterProps = {
+  isHome: boolean
+}
 
-const FooterContainer = styled.footer`
+const FooterContainer = styled.footer<FooterProps>`
   width: 100%;
   height: 30px;
-  color: var(--primary);
+  color: ${props => props.isHome ? 'white' : 'var(--black)'};
   display: flex;
   justify-content: center;
   align-items: center;
