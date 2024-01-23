e21adaf7960d4e8a29f8327dc2e8acd4f8d4d19d
김민지 <blueseablueskyblueme@gmail.com>
2024-01-23 09:45:47 +0900

    [rename] 컴포넌트 명 수정

---
+++ client/src/RootLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet, useLocation } from "react-router-dom"
+import { MainContainer } from "./components/Containers"
+import Footer from "./components/Footer"
+import Header from "./components/Nav"
+import ReactPlayer from "react-player/file";
+
+
+
+export default function RootLayout() {
+  const pathname = useLocation().pathname
+  return (
+    <>
+      <Header />
+      <MainContainer>
+        <Outlet />
+        <Footer />
+        {pathname === '/' && <ReactPlayer className="bg" url="bg.mp4" width="100%" height="100%" playing={true} />}
+      </MainContainer>
+    </>
+  )
+}
+
+
