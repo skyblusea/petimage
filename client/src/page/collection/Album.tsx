58df28c67bb0e79c5b9069238eaa747efab2de5c
김민지 <blueseablueskyblueme@gmail.com>
2024-01-25 13:51:37 +0900

    [feat] 다운로드

--- client/src/page/collection/Album.tsx
+++ client/src/page/collection/Album.tsx
@@ -8,16 +8,49 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
 import { AlbumItem } from "../../types";
 import BaseImgBox from "../../components/Boxes";
+import { IconButton } from "@mui/material";
 
-export default function Album({ data }: { data: AlbumItem}) {
+export default function Album({ data }: { data: AlbumItem }) {
   //TODO : rendering delay
+
+  const onClickHandler = () => {
+    
+    const downloadFile = (url: string) => {
+      fetch(url, { method: 'GET' })
+        .then((res) => {
+          return res.blob();
+        })
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = "파일명";
+          document.body.appendChild(a);
+          a.click();
+          setTimeout((_) => {
+            window.URL.revokeObjectURL(url);
+          }, 60000);
+          a.remove();
+        })
+        .catch((err) => {
+          console.error('err: ', err);
+        });
+    }
+
+    data.outputFiles.forEach((file) => {
+      downloadFile(file)
+    })
+
+  };
   return (
     <AlbumContainer>
       <AlbumHeader>
-        <Typography variant="subtitle1">{new Intl.DateTimeFormat(undefined, {dateStyle:'medium'}).format(new Date(data.createdAt))}</Typography>
-        <DownloadIcon fontSize="large" />
+        <Typography variant="subtitle1">{new Intl.DateTimeFormat(undefined, { dateStyle: 'medium' }).format(new Date(data.createdAt))}</Typography>
+        <IconButton aria-labe="download" onClick={onClickHandler} >
+          <DownloadIcon fontSize="large" />
+        </IconButton>
       </AlbumHeader>
-      <AccordionBox>
+      <AccordionBox TransitionProps={{ unmountOnExit: true }} disabled={!data.status} >
         <AccordionSummary
           expandIcon={<ExpandMoreRoundedIcon fontSize="large" color="petimage" />}
           aria-controls="panel1a-content"
@@ -26,11 +59,11 @@ export default function Album({ data }: { data: AlbumItem}) {
           <Typography variant="h4" color="petimage.main">컨셉사진 / {data.themeName} / {data.outputFiles.length}장</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <Grid container spacing={1}>
-            {data.outputFiles.map((file,idx) => 
-            <Grid key={idx} xs={12/5}>
-              <BaseImgBox square src={file} alt="dog"></BaseImgBox>
-            </Grid>
+          <Grid container spacing={1}>
+            {data.outputFiles.map((file, idx) =>
+              <Grid key={idx} xs={12 / 5}>
+                <BaseImgBox square src={file} alt="dog"></BaseImgBox>
+              </Grid>
             )}
           </Grid>
         </AccordionDetails>
