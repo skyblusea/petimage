c12326e2388b0e49c1e52e35b97ad34299703205
김민지 <blueseablueskyblueme@gmail.com>
2024-01-29 16:16:17 +0900

    [design] 애플로그인 버튼 높이 수정

--- client/src/components/AppleLoginBtn.tsx
+++ client/src/components/AppleLoginBtn.tsx
@@ -1,32 +1,41 @@
+
+import styled from "@emotion/styled";
 import { useEffect } from "react";
 
 export default function AppleLoginBtn() {
 
 
   useEffect(() => {
+    const signInWithApple = async () => { 
     if (typeof window === "undefined" || !window.AppleID) {
       return;
     }
-    AppleID.auth.init({
+    window.AppleID.auth.init({
       clientId: 'petimage.src.com',
       scope: 'name email',
-      redirectURI: 'https://petimage.vercel.app/',
+      redirectURI: 'http://localhost:5173',
       state: 'state',
       nonce: 'nonce',
       usePopup: true
     });
-
-    try {
-      // const data = await AppleID.auth.signIn()
-
-      // Handle successful response.
-    } catch (error) {
-      // Handle error.
-    }
-
-
+    // try {
+    //   const res = await window.AppleID.auth.signIn()
+    //   console.log('res', res)
+    //   // Handle successful response.
+    // } catch (error) {
+    //   // Handle error.
+    // }
+  }
+  signInWithApple()
   }, [])
 
 
-  return <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div>
+
+  return <ButtonWrapper><div id="appleid-signin" className="appleid-signin" data-color="black" data-border="true" data-type="sign in"/></ButtonWrapper>
 }
+
+const ButtonWrapper = styled.div`
+  .appleid-signin{
+    height: 40px; //구글 버튼과 동일하게 맞춤
+  }
+`
--- client/src/page/home/page.tsx
+++ client/src/page/home/page.tsx
@@ -7,6 +7,7 @@ import Logo from '../../assets/logo.svg?react'
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
+import { SvgIcon } from "@mui/material";
 
 
 export default function Home() {
@@ -14,17 +15,19 @@ export default function Home() {
     <SingleSection>
       <ContentWrapper>
         <StyledSymbol />
-        <Box display="flex" justifyContent="center" alignItems="center" sx={{ gap: 'var(--gap-lg)', width: '100%', flexDirection: { xs : 'column', md: 'row' }, color: 'white' }}>
-          <Typography  color="white" variant="h1" sx={{ typography: { xs: 'h3' } }}>
+        <Box display="flex" justifyContent="center" alignItems="center" sx={{ 
+          'svg': {height: '48px'},
+          gap: { xs: 'var(--gap-sm)', md: 'var(--gap-lg)' }, width: '100%', flexDirection: { xs: 'column', md: 'row' }, color: 'white' }}>
+          <Typography color="white" variant="h1" sx={{ typography: { xs: 'h3' }, flexShrink: 0 }}>
             WELCOME TO
           </Typography>
           <Logo />
         </Box>
         <Box>
-          <Typography  color="white" variant="body1" sx={{ typography: { xs: 'body3' } }}>
+          <Typography color="white" variant="body1" sx={{ typography: { xs: 'body3' } }}>
             페티마제는 반려동물 AI 프로필 사진을 생성해주는 서비스입니다.
           </Typography>
-          <Typography  color="white" variant="body1" sx={{ typography: { xs: 'body3' } }}>
+          <Typography color="white" variant="body1" sx={{ typography: { xs: 'body3' } }}>
             페티마제와 함께 반려동물이 뛰어노는 세상으로 여행해볼까요?
           </Typography>
         </Box>
